08dabb64b7449e5b67557a98b4728c85fe96f038
Richard <Richard@FreeBSD12-Dev.local>
2020-01-19 22:18:06 -0500

    post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -1,13 +1,4 @@
 #!/bin/sh -x
-FREEPBX_VER="freepbx-15.0-latest.tgz"
-
-MYSQL_ROOT_PASS=$(openssl rand -base64 20 | md5 | head -c20)
-
-MY_SERVER_NAME=$(hostname)
-IP_ADDRESS=$(ifconfig | grep -E 'inet.[0-9]' | grep -v '127.0.0.1' | awk '{ print $2}')
-
-ASTERISK_USER="asterisk"
-
 # Enable the service
 sysrc apache24_enable="YES"
 sysrc asterisk_enable="YES"
